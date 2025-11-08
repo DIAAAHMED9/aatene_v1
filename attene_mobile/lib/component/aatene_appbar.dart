72cab763b18a1ad6947c4dd82e28898b093a7fb5
diaa <diahkhrh2002@gmail.com>
2025-11-08 06:08:06 -0800

    add edit for app

--- attene_mobile/lib/component/aatene_appbar.dart
+++ attene_mobile/lib/component/aatene_appbar.dart
@@ -2,33 +2,44 @@ import 'package:flutter/material.dart';
 import '../utlis/colors/app_color.dart';
 
 class AateneAppbar extends StatelessWidget {
-  const AateneAppbar({super.key, required this.title});
+  const AateneAppbar({super.key, required this.title, required this.widget});
 
   final dynamic title;
+  final dynamic widget;
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text(
-        title,
-        style: TextStyle(
-          color: AppColors.neutral100,
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
+    return Scaffold(
+      appBar:  AppBar(
+        title: Text(
+          title,
+          style: TextStyle(
+            color: AppColors.neutral100,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: Colors.grey[300],
+            ),
+            child: Icon(Icons.arrow_back, color: AppColors.primary500),
+          ),
         ),
       ),
-      leading: IconButton(
-        onPressed: () {},
-        icon: Container(
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(100),
-            color: Colors.grey[300],
-          ),
-          child: Icon(Icons.arrow_back, color: AppColors.primary500),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: widget(),
         ),
       ),
     );
+
+
   }
 }
---
+++ attene_mobile/lib/view/fot edit /manage_account_store.dart
@@ -0,0 +1,11 @@
+import 'package:attene_mobile/component/aatene_appbar.dart';
+import 'package:flutter/material.dart';
+
+class ManageAccountStore extends StatelessWidget {
+  const ManageAccountStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AateneAppbar(title: "title", widget: Column());
+  }
+}
