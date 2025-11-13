1f9abd6f5ef97ba1dc221a1e6d1312008d98ffa5
diaa <diahkhrh2002@gmail.com>
2025-11-13 01:29:57 -0800

    add controller vendor

---
+++ attene_mobile/lib/view/Control_Panal_Vendor/controler_vindor.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ControlerVindor extends StatelessWidget {
+  const ControlerVindor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
