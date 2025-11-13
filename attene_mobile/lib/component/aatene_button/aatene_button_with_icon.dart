086eb4ee96151d4a9ab464e7b7f61df516d071ef
diaa <diahkhrh2002@gmail.com>
2025-11-13 01:48:03 -0800

    add controller vendor 2

--- attene_mobile/lib/component/aatene_button/aatene_button_with_icon.dart
+++ attene_mobile/lib/component/aatene_button/aatene_button_with_icon.dart
@@ -1,3 +1,4 @@
+import 'package:attene_mobile/utlis/colors/app_color.dart';
 import 'package:flutter/material.dart';
 
 class AateneButtonWithIcon extends StatelessWidget {
@@ -5,6 +6,12 @@ class AateneButtonWithIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          ImageIcon(AssetImage(""), size: 30, color: AppColors.primary400,),
+        ],
+      ),
+    );
   }
 }
