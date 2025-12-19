2cde143774df178005ae66097b701cafc5ddd0f7
DIAAAHMED9 <diahkhrh2002@gmail.com>
2025-12-19 17:58:22 +0200

    edit_problom_build_apk

--- attene_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ attene_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import connectivity_plus
 import file_picker
 import file_selector_macos
 import flutter_inappwebview_macos
-import flutter_local_notifications
 import flutter_secure_storage_macos
 import package_info_plus
 import path_provider_foundation
@@ -26,7 +25,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
