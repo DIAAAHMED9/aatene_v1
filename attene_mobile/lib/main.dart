a0f863f56088c2094a5c1e575daf1175e1a7d2c2
diahk <diahkhrh2002@gmail.com>
2026-01-09 23:16:36 +0200

    start home page

--- attene_mobile/lib/main.dart
+++ attene_mobile/lib/main.dart
@@ -170,7 +170,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       getPages: [
-        GetPage(name: '/', page: () => HomePage()),
+        GetPage(name: '/', page: () => SplashScreen()),
         GetPage(name: '/onboarding', page: () => const Onbording()),
         GetPage(name: '/start_login', page: () => const StartLogin()),
         GetPage(name: '/login', page: () => Login()),
